33be365d7470675de6403ca5b9d7452e6b6147e9
Paul <paul21479@gmail.com>
2022-03-03 23:32:36 +1300

    changing readme to tk, in progress to change code to tk

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class MyWindow(Gtk.Window):
         self.box = Gtk.Box(spacing=6)
         self.add(self.box)
 
-        self.button1 = Gtk.Button(label="Click Here")
+        self.button1 = Gtk.Button(label="Hello")
         self.button1.connect("clicked", self.on_button1_clicked)
         self.box.pack_start(self.button1, True, True, 0)
 
