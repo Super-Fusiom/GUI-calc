ca681d8bca994eb5cc3b35737a45042a5a6c3fa5
Paul <paul21479@gmail.com>
2022-03-05 23:25:28 +1300

    Changed from GTK to tkinter due to compatibility.

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-# Gi is required to use GTK
+import tkinter as tk
+from tkinter import *
 
-import gi
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
-#The window itself
-class MyWindow(Gtk.Window):
-    def __init__(self):
-        super().__init__(title="Hello World")
+def hello(greeting):
+    print(greeting)
 
-        self.box = Gtk.Box(spacing=6)
-        self.add(self.box)
+root = tk.Tk()
+root.title("OOOOOH")
 
-        self.button1 = Gtk.Button(label="Hello")
-        self.button1.connect("clicked", self.on_button1_clicked)
-        self.box.pack_start(self.button1, True, True, 0)
+Hellobtn = tk.Button(root, text="Hello", command=lambda:hello("Hello"))
+Byebtn = tk.Button(root, text="Bye", command=lambda:hello("Bye"))
+Hellobtn.pack()
+Byebtn.pack()
 
-        self.button2 = Gtk.Button(label="Goodbye")
-        self.button2.connect("clicked", self.on_button2_clicked)
-        self.box.pack_start(self.button2, True, True, 0)
-
-    def on_button1_clicked(self, widget):
-        print('Hello')
-
-    def on_button2_clicked(self, widget):
-        print("Goodbye")    
-# Loop the window
-
-win = MyWindow()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+root.mainloop()
